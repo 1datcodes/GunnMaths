0840ebba200c7e44a44a06d88341ea0e1263b3d9
Michi Tanaka <tanaka.michi@icloud.com>
2024-05-22 22:31:40 -0700

    Updated Testing files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders analysis page on / route', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+  const linkElement = screen.getByText(/ANALYSIS/i);
   expect(linkElement).toBeInTheDocument();
 });
