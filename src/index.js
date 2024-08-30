4c775f819f7eb2b792da96d6110794efb66d38a6
Kaichengxdd <kaichengluo888@gmail.com>
2024-08-30 12:27:46 -0700

    i made it better.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-window.onload = function() {
-  const root = document.getElementById('root');
+window.onload = function () {
+  const root = document.getElementById("root");
   createRoot(root).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 };
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
