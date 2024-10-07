54521dac27306b3af2d2600586251852ff82305a
Michi Tanaka <tanaka.michi@icloud.com>
2024-10-07 15:50:19 -0700

    Added menu button for mobile

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,45 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import HomeButton from "./HomeButton";
 import Upload from "./UploadButton";
 
 function Header({ headerTitle, description }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="Header">
+      <div className={`Mobile-Menu ${menuOpen ? "Open" : "Close"}`}>
+        <Link to="/" className="Mobile-Menu-Item">
+          Home
+        </Link>
+        <Link to="" className="Mobile-Menu-Item">
+          Upload Test
+        </Link>
+        <Link to="/about#top" className="Mobile-Menu-Item">
+          About Us
+        </Link>
+        <Link to="/about#contact" className="Mobile-Menu-Item">
+          Contact Us
+        </Link>
+        <Link to="/update" className="Mobile-Menu-Item">
+          Update Log
+        </Link>
+      </div>
+      
       <div className="Main-Header">
+        <button className="Menu-Button" onClick={toggleMenu}>
+          ☰
+        </button>
         <HomeButton />
         <div className="Texts">
           <h1 className="Title">{headerTitle}</h1>
           <p className="Description">{description}</p>
         </div>
-        {/* Add an upload button */}
         <Upload />
       </div>
       <div className="Sub-Header">
