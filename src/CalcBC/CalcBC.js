b8769bed83e90404161e85a6ddc1549cd47f98bf
Michi Tanaka <tanaka.michi@icloud.com>
2025-04-16 14:02:40 -0700

    added calc units and unit images

--- src/CalcBC/CalcBC.js
+++ src/CalcBC/CalcBC.js
@@ -3,7 +3,82 @@ import "./CalcBC.css";
 import Header from "../Header/Header";
 import BackButton from "../BackButton";
 
-const units = [];
+// import all images
+import unit1 from "./calc-images/limits.png";
+import unit2 from "./calc-images/derivative.png";
+import unit3 from "./calc-images/more-derivative.png";
+import unit4 from "./calc-images/app-derivative.png";
+import unit5 from "./calc-images/more-app-derivative.png";
+import unit6 from "./calc-images/integral.png";
+import unit7 from "./calc-images/diff-eq.png";
+import unit8 from "./calc-images/app-int.png";
+import unit9 from "./calc-images/param.png";
+import unit10 from "./calc-images/series.png";
+import midterm from "./calc-images/midterm.png";
+import apreview from "./calc-images/apreview.png";
+
+const units = [
+  {
+    text: "Unit 1: Limits and Continuity",
+    url: "/calcBC/unit1",
+    img: unit1,
+  },
+  {
+    text: "Unit 2: Derivative",
+    url: "/calcBC/unit2",
+    img: unit2,
+  },
+  {
+    text: "Unit 3: More Derivatives",
+    url: "/calcBC/unit3",
+    img: unit3,
+  },
+  {
+    text: "Unit 4: Application of Derivatives",
+    url: "/calcBC/unit4",
+    img: unit4,
+  },
+  {
+    text: "Unit 5: More Application",
+    url: "/calcBC/unit5",
+    img: unit5,
+  },
+  {
+    text: "Unit 6: Integral",
+    url: "/calcBC/unit6",
+    img: unit6,
+  },
+  {
+    text: "Differential Equations",
+    url: "/calcBC/unit7",
+    img: unit7,
+  },
+  {
+    text: "Unit 8: Application of Integrals",
+    url: "/calcBC/unit8",
+    img: unit8,
+  },
+  {
+    text: "Unit 9: Parametric and Polar",
+    url: "/calcBC/unit9",
+    img: unit9,
+  },
+  {
+    text: "Unit 10: Sequences and Series",
+    url: "/calcBC/unit10",
+    img: unit10,
+  },
+  {
+    text: "Midterms and Finals",
+    url: "/calcBC/midterms",
+    img: midterm,
+  },
+  {
+    text: "AP Test Review",
+    url: "/calcBC/apreview",
+    img: apreview,
+  }
+];
 
 function CalcBC() {
   return (
@@ -12,10 +87,6 @@ function CalcBC() {
 
       <div className="Units">
         <BackButton />
-        <h2>Coming soon...</h2>
-        <p>
-          <small>(It's never coming...)</small>
-        </p>
         <div className="icon-grid">
           {units.map((unit, index) => (
             <div className="units" key={index}>
