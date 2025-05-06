e919b59b6f88498bef2b5c92271853c0cc7844ff
Michi Tanaka <tanaka.michi@icloud.com>
2025-05-06 10:33:56 -0700

    accordion prototype

--- src/Courses/Unit.jsx
+++ src/Courses/Unit.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 import BackButton from "../BackButton";
 
 function Unit({ resources }) {
+  const [openCategories, setOpenCategories] = useState({});
+  const [openTypes, setOpenTypes] = useState({});
+
+  const toggleCategory = (category) => {
+    setOpenCategories((prev) => ({
+      ...prev,
+      [category]: !prev[category],
+    }))
+  };
+  const toggleType = (category, type) => {
+    if (openCategories[category]) {
+      setOpenTypes((prev) => ({
+        ...prev,
+        [`${category}-${type}`]: !prev[`${category}-${type}`],
+      }));
+    };
+  };
+
   const organizedResources = resources.reduce((accumulator, resource) => {
     if (!accumulator["Tests"]) {
       accumulator["Tests"] = [];
@@ -56,6 +75,34 @@ function Unit({ resources }) {
       <Header />
       <div id="resources" className="">
         <BackButton />
+        {Object.keys(organizedResources).map((category, index) => {
+          console.log(category);
+          return (
+            <div key={index} id="category" className="" onClick={() => toggleCategory(category)}>
+              <h1>{category}</h1>
+              {openCategories[category] && (
+                <div id="container" className="">
+                  {Object.keys(organizedResources[category]).map((type, index) => {
+                    return (
+                      <div key={index} id="type" className="" onClick={() => toggleType(category, type)}>
+                        <h1>{type}</h1>
+                        {openTypes[`${category}-${type}`] && (
+                        <div id="resources" className="">
+                          {organizedResources[category][type].map((resource, index) => {
+                            return (
+                              <a href={resource.file} target="_blank" rel="noopener noreferrer" key={index} className="">{resource.name}</a>
+                            )
+                          })}
+                        </div>
+                        )}
+                      </div>
+                    )
+                  })}
+                </div>
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   );
