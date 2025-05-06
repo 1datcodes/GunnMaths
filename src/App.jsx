8a1fe88ecf9f33aea0e2bdd3e414ce69fcaf606b
michi <tanakamichi7@gmail.com>
2025-05-05 19:04:02 -0700

    in the process of moving file accumulator to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,39 @@ const courses = Object.keys(courseModules).map((key) => {
   };
 });
 
+const unitModules = import.meta.glob(["./Courses/**/*", "!**/*.jsx", "!**/*images"], { eager: true }); // dynamically import all files regardless of the depth
+const groupedUnits = Object.keys(unitModules).reduce(
+  (accumulator, filePath) => {
+    // accumulator is the object that will hold the units, filePath is the path to the file
+    const courseName = filePath.split("/")[2]; // Get the course name
+    if (!accumulator[courseName]) {
+      accumulator[courseName] = []; // Initialize an array for the folder if it doesn't exist already
+    }
+    const unitName = filePath.split("/")[4]; // Get the unit name
+    if (!accumulator[courseName][unitName]) {
+      accumulator[courseName][unitName] = []; // Initialize an array for the unit if it doesn't exist already
+    }
+    accumulator[courseName][unitName].push({
+      name: filePath.split("/").pop().replace(".pdf", ""), // Get the file name without the extension
+      url: `/${courseName.toLowerCase()}/${unitName.toLowerCase()}`, // Get the URL for the unit
+      path: filePath, // Get the full path to the file
+    });
+
+    return accumulator; // Return the accumulator for the next iteration
+  },
+  {},
+); // initial value is an empty object
+console.log(groupedUnits);
+
+const units = Object.keys(groupedUnits).map((unit) => {
+  return {
+    name: unit,
+    path: `/analysis/${unit.toLowerCase()}`,
+    documents: groupedUnits[unit],
+  };
+});
+console.log(units);
+
 function App() {
   return (
     <Router>
