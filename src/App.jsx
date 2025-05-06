a38e9c6051b78750cd99e60d10d0b375cb06a68f
michi <tanakamichi7@gmail.com>
2025-05-05 22:16:31 -0700

    migrated course page into course.jsx and routed everything dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./HomePage";
 import About from "./Header/About";
+import Course from "./Courses/Course";
 
-const courseModules = import.meta.glob("./Courses/*/*.jsx", { eager: true });
-const courses = Object.keys(courseModules).map((key) => {
-  const componentName = key.split("/").pop().replace(".jsx", "");
-  return {
-    path: `/${componentName.toLowerCase()}/*`,
-    component: courseModules[key].default,
-  };
-});
-
-const unitModules = import.meta.glob(["./Courses/**/*", "!**/*.jsx", "!**/*images"], { eager: true }); // dynamically import all files regardless of the depth
-const groupedUnits = Object.keys(unitModules).reduce(
+const courseModules = import.meta.glob(["./Courses/**/*", "!**/*.jsx"], { eager: true }); // dynamically import all files regardless of the depth
+const groupedCourses = Object.keys(courseModules).reduce(
   (accumulator, filePath) => {
     // accumulator is the object that will hold the units, filePath is the path to the file
     const courseName = filePath.split("/")[2]; // Get the course name
     if (!accumulator[courseName]) {
       accumulator[courseName] = []; // Initialize an array for the folder if it doesn't exist already
     }
+    accumulator[courseName]["path"] = `/${courseName.toLowerCase()}`; // Get the path for the course
+    if (filePath.includes("images")) {
+      if (!accumulator[courseName]["images"]) {
+        accumulator[courseName]["images"] = []; // Initialize an array for the images if it doesn't exist already
+      }
+      accumulator[courseName]["images"].push({
+        image: courseModules[filePath].default, // Get the full path to the image
+      })
+      return accumulator;
+    }
     const unitName = filePath.split("/")[4]; // Get the unit name
     if (!accumulator[courseName][unitName]) {
       accumulator[courseName][unitName] = []; // Initialize an array for the unit if it doesn't exist already
     }
+    accumulator[courseName][unitName]["name"] = unitName; // Get the unit name
+    accumulator[courseName][unitName]["path"] = `/${courseName.toLowerCase()}/${unitName.toLowerCase()}`; // Get the path for the unit
     accumulator[courseName][unitName].push({
       name: filePath.split("/").pop().replace(".pdf", ""), // Get the file name without the extension
-      url: `/${courseName.toLowerCase()}/${unitName.toLowerCase()}`, // Get the URL for the unit
       path: filePath, // Get the full path to the file
     });
 
@@ -33,16 +36,6 @@ const groupedUnits = Object.keys(unitModules).reduce(
   },
   {},
 ); // initial value is an empty object
-console.log(groupedUnits);
-
-const units = Object.keys(groupedUnits).map((unit) => {
-  return {
-    name: unit,
-    path: `/analysis/${unit.toLowerCase()}`,
-    documents: groupedUnits[unit],
-  };
-});
-console.log(units);
 
 function App() {
   return (
@@ -51,12 +44,18 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
 
-        {courses.map((course, index) => (
-          <Route
-            key={index}
-            path={course.path}
-            element={<course.component />}
-          />
+        {Object.keys(groupedCourses).map((index) => (
+          <>
+            <Route
+              key={index}
+              path={groupedCourses[index]["path"]}
+              element={<Course units={groupedCourses[index]} />}
+            />
+            <Route
+              key={index}
+              path={`${groupedCourses[index]["path"]}/:unit`}
+              element={<div></div>} />
+          </>
         ))}
       </Routes>
     </Router>
---
+++ src/Courses/Course.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+import Header from "../Header/Header";
+import BackButton from "../BackButton";
+
+function Course({ units }) {
+    const coverImages = units["images"];
+    
+    return (
+        <div id="content" className="mb-[3rem]">
+        <Header />
+        <div id="units" className="py-[2.5rem] px-[3.75rem]">
+            <BackButton />
+            <div
+            id="quick-title"
+            className="text-black pt-[3rem] pl-[3.75rem] text-2xl font-bold"
+            >
+                <h2 className="mb-[1rem]">Analysis Resources</h2>
+                </div>
+                <div
+                id="grid"
+                className="py-[0.625rem] px-[3.125rem] grid grid-cols-3 gap-[6rem]"
+                >
+                {Object.keys(units).map((unit, index) => {
+                    if (unit === "images" || unit === "path") {
+                        return null; // Skip the images unit
+                    }
+                    
+                    return (
+                    <div
+                        id="unit"
+                        key={index}
+                        onClick={() => (window.location.href = units[unit]["path"])}
+                        className="group relative w-full flex flex-row justify-end items-center gap-[1.25rem] z-0 transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-102"
+                    >
+                        <Link
+                        to={units[unit]["path"]}
+                        id="unit-link"
+                        className="w-[15rem] h-[15rem] flex flex-row justify-between items-center rounded-[1rem] overflow-hidden"
+                        >
+                        <div
+                            id="gradient-overlay"
+                            className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-[#023047] from-25% via-70% to-transparent z-10 rounded-[inherit] shadow-sm shadow-black pointer-events-none flex flex-col justify-start items-start"
+                        >
+                            <h1
+                            id="unit-title"
+                            className="text-white z-20 pl-[1.5rem] pr-[1rem] my-[1.5rem] text-shadow-sm text-3xl font-bold group-hover:underline decoration-highlight"
+                            >
+                            {units[unit]["name"]}
+                            </h1>
+                            <p
+                            id="unit-description"
+                            className="text-white z-20 pl-[1.5rem] pr-[1rem] my-[1rem] text-shadow-sm group-hover:underline decoration-highlight"
+                            >
+                            {} Tests and Quizzes
+                            </p>
+                        </div>
+                        <img
+                            id="unit-image"
+                            src={coverImages[index - 2].image} // Adjusted index to skip images and path
+                            alt={"image"}
+                            className="w-full h-[15rem] object-cover relative"
+                        />
+                        </Link>
+                    </div>
+                    );
+                })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Course;
