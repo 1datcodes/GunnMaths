214b4f9a39905bba823f31025e39e66956e0c9dc
1datcodes <tanakamichi7@gmail.com>
2025-05-08 11:03:36 -0700

    animation needs tweaking

--- src/Courses/Unit.jsx
+++ src/Courses/Unit.jsx
@@ -91,34 +91,39 @@ function Unit({ resources }) {
                 <button
                   id="dropdown"
                   onClick={() => handleClick(category)}
-                  className="items-center bg-gray-200 border-1 rounded-sm text-black cursor-pointer flex text-base gap-1.5 justify-start w-full p-4.5 text-left"
+                  className="items-center bg-gray-100 border-1 rounded-sm text-black cursor-pointer flex text-base gap-1.5 justify-start w-full p-4.5 text-left"
                 >
                   {category}{" "}
-                  <span id="arrow" className={`transition-all duration-300 ease-in-out ${activeIndex[category] ? "rotate-90" : "rotate-none"}`}>
+                  <span
+                    id="arrow"
+                    className={`transition-all duration-300 ease-in-out ${activeIndex[category] ? "rotate-90" : "rotate-none"}`}
+                  >
                     ▶
                   </span>
                 </button>
-                <div id="dropdown-content" className={`overflow-hidden transition-all duration-300 ease-in-out w-9/10 self-end ${
-                  activeIndex[category] ? "max-h-screen" : "max-h-0"
-                }`}>
+                <div
+                  id="dropdown-content"
+                  className={`overflow-hidden transition-all duration-300 ease-in-out w-95/100 self-end ${
+                    activeIndex[category] ? "max-h-screen" : "max-h-0"
+                  }`}
+                >
                   {Object.keys(organizedResources[category]).map(
                     (subcategory, subIndex) => {
                       return (
-                        <div
-                          key={subIndex}
-                          className="mt-1"
-                        >
+                        <div key={subIndex} className="mt-1">
                           <button
                             id="dropdown"
                             key={subIndex}
                             onClick={() => handleClick(category, subcategory)}
-                            className="items-center bg-gray-200 border-1 rounded-sm text-black cursor-pointer flex text-base gap-1.5 justify-start w-full p-4.5 text-left"
+                            className="items-center bg-gray-100 border-1 rounded-sm text-black cursor-pointer flex text-base gap-1.5 justify-start w-full p-4.5 text-left"
                           >
                             {subcategory}{" "}
                             <span
                               id="arrow"
                               className={`transform transition-transform duration-300 ease-in-out ${
-                                activeIndex[category]?.[subcategory] ? "rotate-90" : "rotate-none"
+                                activeIndex[category]?.[subcategory]
+                                  ? "rotate-90"
+                                  : "rotate-none"
                               }`}
                             >
                               ▶
@@ -128,11 +133,14 @@ function Unit({ resources }) {
                             id="dropdown-content"
                             className={`flex justify-end overflow-hidden transition-all duration-300 ease-in-out ${
                               activeIndex[category]?.[subcategory]
-                                ? "max-h-screen"
-                                : "max-h-0"
+                                ? "opacity-100 scale-y-100 pointer-events-none"
+                                : "opacity-0 scale-y-0 pointer-events-none"
                             }`}
                           >
-                            <div id="files" className="w-9/10 border-1 my-1 rounded-sm">
+                            <div
+                              id="files"
+                              className="w-95/100 border-1 my-1 rounded-sm"
+                            >
                               {organizedResources[category][subcategory].map(
                                 (resource, resourceIndex) => {
                                   const active = activeIndex[category]
@@ -142,18 +150,24 @@ function Unit({ resources }) {
                                     : false;
 
                                   return active ? (
-                                    <div key={resourceIndex} onClick={() => window.open(resource.file, "_blank", "noopener, noreferrer")} className="px-[1rem] py-[0.25rem] hover:bg-gray-100 hover:cursor-pointer hover:underline">
+                                    <div
+                                      key={resourceIndex}
+                                      onClick={() =>
+                                        window.open(
+                                          resource.file,
+                                          "_blank",
+                                          "noopener, noreferrer",
+                                        )
+                                      }
+                                      className="px-[1rem] py-[0.25rem] hover:bg-gray-100 hover:cursor-pointer hover:underline"
+                                    >
                                       <a
-                                        href={
-                                          resource.file
-                                        }
+                                        href={resource.file}
                                         className=""
                                         target="_blank"
                                         rel="noopener noreferrer"
                                       >
-                                        {
-                                          resource.name
-                                        }
+                                        {resource.name}
                                       </a>
                                     </div>
                                   ) : null;
